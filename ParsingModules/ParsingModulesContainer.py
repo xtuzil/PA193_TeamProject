2187077106f85dc35e1cb241e187cb9fc047d46b
Matěj Tužil <matej.tuzil@Matejs-MacBook-Pro.local>
2021-04-03 20:14:27 +0200

    Bibliography modul added

--- ParsingModules/ParsingModulesContainer.py
+++ ParsingModules/ParsingModulesContainer.py
@@ -2,6 +2,7 @@ from typing import Type, List
 
 from ParsingModules.IParsingModule import IParsingModule
 from ParsingModules.VersionsParsingModule import VersionsParsingModule
+from ParsingModules.BibliographyParsingModule import BibliographyParsingModule
 
 
 class ParsingModulesContainer:
@@ -9,4 +10,5 @@ class ParsingModulesContainer:
     def get_parsing_modules() -> List[Type[IParsingModule]]:
         return [
             VersionsParsingModule,
+            BibliographyParsingModule,
         ]
