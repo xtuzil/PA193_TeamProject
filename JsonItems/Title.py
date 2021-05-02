46e8c6573ebc0efde8aa6565d1a690261679aa8d
Matěj Tužil <matej.tuzil@Matejs-MacBook-Pro.local>
2021-05-02 10:49:15 +0200

    Create review branch

--- JsonItems/Title.py
+++
@@ -1,9 +0,0 @@
-from JsonItems.IJsonItem import IJsonItem
-
-
-class Title(IJsonItem):
-    def __init__(self, title: str):
-        self._title = title
-
-    def get_structure_for_json(self):
-        return self._title
